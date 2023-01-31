282e96082289e7b053dd3c4fb9eed2b4bb46916e
ten <tiktaalik7@gmail.com>
2023-01-31 15:47:36 +0900

    initial commit

---
+++ src/test/java/de_identification_verifier/AppTest.java
@@ -0,0 +1,20 @@
+package de_identification_verifier;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
